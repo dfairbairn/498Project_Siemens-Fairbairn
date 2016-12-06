d1a2a1e901d0fa8ffebcb8a82cb2a7826a29e8ce
David Fairbairn <davidtfairbairn@gmail.com>
2016-12-05 20:23:03 -0600

    Updated plot_cluster_stats.py so that it outputs 'outcome' plots and saves them to a local directory that it checks for /creates

--- cluster.py
+++ cluster.py
@@ -4,27 +4,6 @@ from sklearn.metrics.cluster import calinski_harabaz_score
 import numpy as np
 import pandas as pd
 
-def try_kmeans(fnamecsv,n_clusters=8):
-    """ 
-    Attempts a kmeans clustering on a dataset in a file, returning the kmeans object
-    """
-    from sklearn.cluster import KMeans
-    import numpy as np
-    df = pd.read_csv(fnamecsv)
-    #TODO check for successful read
-    variables = (df.as_matrix())[:,1:] #exclude first column which is pandas indices
-    # for j in columns of variables
-    #   modify each entry by subtracting mean of this column, dividing by stdev of this column
-    # Could be using pandas' built-in means and standard deviation functions if I think of a nice way to do this for each column
-    for j in range(len(variables[0,:])): #ugly way of looping over columns
-        variables[:,j] = ( variables[:,j] - np.mean(variables[:,j]))/np.std(variables[:,j])
-    
-    # now each row of variables can be used for euclidean distance in kmeans
-    k = KMeans(n_clusters=n_clusters, n_init=30,n_jobs=3).fit(variables)
-    print "Labels: ",k.labels_
-    print "Cluster centers: ",k.cluster_centers_
-    return k
-
 def cluster_number_study(n=50):
     """ Check out some basic cluster metrics for different cluster sizes. """
 
@@ -92,29 +71,29 @@ def put_in_pandas_old(fnamecsv='./AL_pchange_vars.csv'):
     df = pd.DataFrame(tmp,columns=['INN_CT','RBI_CT','PA_BALL_CT','EVENT_OUTS_CT','BAT_DEST_ID','SCORE_DIFF','PIT_COUNT','LABEL'])
     return df 
 
-def put_in_pandas(fnamecsv='./AL_pchange_vars.csv'):
+def put_in_pandas(fnamecsv='./AL_pchange_vars_wins.csv'):
     """ 
     Performs clustering on a dataframe in a csv file and returns a dataframe 
     of the events with their cluster labeling. 
     """
     tmp_df = pd.read_csv(fnamecsv)
-    tmp_df2 = tmp_df.loc[:][['GAME_ID','EVENT_ID','INN_CT','RBI_CT','PA_BALL_CT','EVENT_OUTS_CT','BAT_DEST_ID','SCORE_DIFF','PIT_COUNT']]
+    tmp_df2 = tmp_df.loc[:][['GAME_ID','EVENT_ID','TEAM_WINS','INN_CT','RBI_CT','PA_BALL_CT','EVENT_OUTS_CT','BAT_DEST_ID','SCORE_DIFF','PIT_COUNT']]
 
     variables = tmp_df2.as_matrix()
-    normd_vars = (variables.copy()[:,2:]).astype(float) # Leave out the Game ID and Event ID variables
+    normd_vars = (variables.copy()[:,3:]).astype(float) # Leave out the Game ID and Event ID variables
     for j in range(len(normd_vars[0,:])): # Ugly way of looping over columns
         normd_vars[:,j] = ( normd_vars[:,j] - np.mean(normd_vars[:,j]))/np.std(normd_vars[:,j])
     k = KMeans(n_clusters=3, n_init=100, n_jobs=3).fit(normd_vars)
     labels_array = np.array([k.labels_])
     tmp = np.concatenate([variables,labels_array.T],axis=1)
-    df = pd.DataFrame(tmp,columns=['GAME_ID','EVENT_ID','INN_CT','RBI_CT','PA_BALL_CT','EVENT_OUTS_CT','BAT_DEST_ID','SCORE_DIFF','PIT_COUNT','LABEL'])
+    df = pd.DataFrame(tmp,columns=['GAME_ID','EVENT_ID','TEAM_WINS','INN_CT','RBI_CT','PA_BALL_CT','EVENT_OUTS_CT','BAT_DEST_ID','SCORE_DIFF','PIT_COUNT','LABEL'])
     return df 
 
 if __name__=="__main__":
     #X = np.array([[30, 50, 80,20], [40, 70, 50, 23], [1, 0 ,2, 4], [5,8,8,0]])
     #kmeans = KMeans(n_clusters=2, random_state=0).fit(X)
 
-    fnamecsv = './AL_pchange_vars.csv'
+    fnamecsv = './AL_pchange_vars_wins.csv'
     df = pd.read_csv(fnamecsv)
 
     labeled_df = put_in_pandas(fnamecsv)
--- plot_cluster_stats.py
+++ plot_cluster_stats.py
@@ -3,6 +3,10 @@ import matplotlib.pyplot as plt
 
 def plot_clusterstats(labeled_vars):
     """ """
+    import os
+    if not os.path.isdir('./cluster_characterization'):
+        print 'Creating an output directory for the images..'
+        os.system('mkdir ./cluster_characterization')
 
     print "Number in cluster 0: ",len(labeled_vars.loc[labeled_vars['LABEL']==0.0])
     print "Number in cluster 1: ",len(labeled_vars.loc[labeled_vars['LABEL']==1.0])
@@ -18,7 +22,7 @@ def plot_clusterstats(labeled_vars):
     plt.title('Score Difference vs. Cluster')
     plt.xlabel('Score Difference')
     plt.ylabel('Number of Occurrences')
-    plt.savefig('../cluster_characterization/cluster_scorediff.png')
+    plt.savefig('./cluster_characterization/cluster_scorediff.png')
     plt.gcf().clear()
 
     # *********************************
@@ -32,7 +36,7 @@ def plot_clusterstats(labeled_vars):
     plt.xlabel('Pitch Count')
     plt.ylabel('Number of Occurrences')
     #plt.show()
-    plt.savefig('../cluster_characterization/cluster_pitch.png')
+    plt.savefig('./cluster_characterization/cluster_pitch.png')
     plt.gcf().clear()
 
     # *********************************
@@ -45,7 +49,7 @@ def plot_clusterstats(labeled_vars):
     plt.title('Inning vs. Cluster')
     plt.xlabel('Inning')
     plt.ylabel('Number of Occurrences')
-    plt.savefig('../cluster_characterization/cluster_inning.png')
+    plt.savefig('./cluster_characterization/cluster_inning.png')
     plt.gcf().clear()
 
     # *********************************
@@ -55,21 +59,21 @@ def plot_clusterstats(labeled_vars):
     plt.title('RBI Count Distribution for Cluster 0')
     plt.xlabel('RBI Count')
     plt.ylabel('Number of Occurrences')
-    plt.savefig('../cluster_characterization/cluster0_RBI.png')
+    plt.savefig('./cluster_characterization/cluster0_RBI.png')
     plt.gcf().clear()
 
     pd.value_counts(labeled_vars.loc[labeled_vars['LABEL']==1.0]['RBI_CT']).sort_index().plot(kind='bar',label='Cluster 1')
     plt.title('RBI Count Distribution for Cluster 1')
     plt.xlabel('RBI Count')
     plt.ylabel('Number of Occurrences')
-    plt.savefig('../cluster_characterization/cluster1_RBI.png')
+    plt.savefig('./cluster_characterization/cluster1_RBI.png')
     plt.gcf().clear()
 
     pd.value_counts(labeled_vars.loc[labeled_vars['LABEL']==2.0]['RBI_CT']).sort_index().plot(kind='bar',label='Cluster 2')
     plt.title('RBI Count Distribution for Cluster 2')
     plt.xlabel('RBI Count')
     plt.ylabel('Number of Occurrences')
-    plt.savefig('../cluster_characterization/cluster2_RBI.png')
+    plt.savefig('./cluster_characterization/cluster2_RBI.png')
     plt.gcf().clear()
 
     # *********************************
@@ -79,21 +83,21 @@ def plot_clusterstats(labeled_vars):
     plt.title('Outs Count Distribution for Cluster 0')
     plt.xlabel('Outs Count')
     plt.ylabel('Number of Occurrences')
-    plt.savefig('../cluster_characterization/cluster0_OUT.png')
+    plt.savefig('./cluster_characterization/cluster0_OUT.png')
     plt.gcf().clear()
 
     pd.value_counts(labeled_vars.loc[labeled_vars['LABEL']==1.0]['EVENT_OUTS_CT']).sort_index().plot(kind='bar',label='Cluster 1')
     plt.title('Outs Count Distribution for Cluster 1')
     plt.xlabel('Outs Count')
     plt.ylabel('Number of Occurrences')
-    plt.savefig('../cluster_characterization/cluster1_OUT.png')
+    plt.savefig('./cluster_characterization/cluster1_OUT.png')
     plt.gcf().clear()
 
     pd.value_counts(labeled_vars.loc[labeled_vars['LABEL']==2.0]['EVENT_OUTS_CT']).sort_index().plot(kind='bar',label='Cluster 2')
     plt.title('Outs Count Distribution for Cluster 2')
     plt.xlabel('Outs Count')
     plt.ylabel('Number of Occurrences')
-    plt.savefig('../cluster_characterization/cluster2_OUT.png')
+    plt.savefig('./cluster_characterization/cluster2_OUT.png')
     plt.gcf().clear()
 
     # *********************************
@@ -103,21 +107,21 @@ def plot_clusterstats(labeled_vars):
     plt.title('Ball Count Distribution for Cluster 0')
     plt.xlabel('Ball Count')
     plt.ylabel('Number of Occurrences')
-    plt.savefig('../cluster_characterization/cluster0_BALL.png')
+    plt.savefig('./cluster_characterization/cluster0_BALL.png')
     plt.gcf().clear()
 
     pd.value_counts(labeled_vars.loc[labeled_vars['LABEL']==1.0]['PA_BALL_CT']).sort_index().plot(kind='bar',label='Cluster 1')
     plt.title('Ball Count Distribution for Cluster 1')
     plt.xlabel('Ball Count')
     plt.ylabel('Number of Occurrences')
-    plt.savefig('../cluster_characterization/cluster1_BALL.png')
+    plt.savefig('./cluster_characterization/cluster1_BALL.png')
     plt.gcf().clear()
 
     pd.value_counts(labeled_vars.loc[labeled_vars['LABEL']==2.0]['PA_BALL_CT']).sort_index().plot(kind='bar',label='Cluster 2')
     plt.title('Ball Count Distribution for Cluster 2')
     plt.xlabel('Ball Count')
     plt.ylabel('Number of Occurrences')
-    plt.savefig('../cluster_characterization/cluster2_BALL.png')
+    plt.savefig('./cluster_characterization/cluster2_BALL.png')
     plt.gcf().clear()
 
     # *********************************
@@ -127,21 +131,48 @@ def plot_clusterstats(labeled_vars):
     plt.title('Batter Destination (Base #) for Cluster 0')
     plt.xlabel('Batter Destination (Base #)')
     plt.ylabel('Number of Occurrences')
-    plt.savefig('../cluster_characterization/cluster0_BATDEST.png')
+    plt.savefig('./cluster_characterization/cluster0_BATDEST.png')
     plt.gcf().clear()
 
     pd.value_counts(labeled_vars.loc[labeled_vars['LABEL']==1.0]['BAT_DEST_ID']).sort_index().plot(kind='bar',label='Cluster 1')
     plt.title('Batter Destination (Base #) for Cluster 1')
     plt.xlabel('Batter Destination (Base #)')
     plt.ylabel('Number of Occurrences')
-    plt.savefig('../cluster_characterization/cluster1_BATDEST.png')
+    plt.savefig('./cluster_characterization/cluster1_BATDEST.png')
     plt.gcf().clear()
 
     pd.value_counts(labeled_vars.loc[labeled_vars['LABEL']==2.0]['BAT_DEST_ID']).sort_index().plot(kind='bar',label='Cluster 2')
     plt.title('Batter Destination (Base #) for Cluster 2')
     plt.xlabel('Batter Destination (Base #)')
     plt.ylabel('Number of Occurrences')
-    plt.savefig('../cluster_characterization/cluster2_BATDEST.png')
+    plt.savefig('./cluster_characterization/cluster2_BATDEST.png')
+    plt.gcf().clear()
+
+    # *********************************
+    # ******** WIN/LOSS RATIO *********
+    # *********************************
+   
+    pd.value_counts(labeled_vars.loc[labeled_vars['LABEL']==0.0]['TEAM_WINS']).sort_index().plot(kind='bar',label='Cluster 0')
+    plt.title('Win Distribution for Cluster 0')
+    plt.xlabel('Loss or Win')
+    plt.ylabel('Number of Occurrences')
+    plt.savefig('./cluster_characterization/outcomes_cluster0.png')
     plt.gcf().clear()
 
 
+    pd.value_counts(labeled_vars.loc[labeled_vars['LABEL']==1.0]['TEAM_WINS']).sort_index().plot(kind='bar',label='Cluster 1')
+    plt.title('Win Distribution for Cluster 1')
+    plt.xlabel('Loss or Win')
+    plt.ylabel('Number of Occurrences')
+    plt.savefig('./cluster_characterization/outcomes_cluster1.png')
+    plt.gcf().clear()
+
+
+    pd.value_counts(labeled_vars.loc[labeled_vars['LABEL']==2.0]['TEAM_WINS']).sort_index().plot(kind='bar',label='Cluster 2')
+    plt.title('Win Distribution for Cluster 2')
+    plt.xlabel('Loss or Win')
+    plt.ylabel('Number of Occurrences')
+    plt.savefig('./cluster_characterization/outcomes_cluster2.png')
+    plt.gcf().clear()
+
+    
